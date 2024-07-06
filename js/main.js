8bc9e8e9375817976f38f466935a9f1ba9e83289
MaganaTony <therealanthonymagana>
2024-07-05 21:55:40 -0600

    Added relevant filter

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Method GET to get all users
 const getAllUsers = async () => {
   const response = await fetch(
     "https://js-7259a-default-rtdb.firebaseio.com/.json",
@@ -10,18 +11,29 @@ const getAllUsers = async () => {
   );
   const data = await response.json();
   let keysArray = Object.keys(data);
-  let usersArray = keysArray.map((key /*user1 || user2 || -a-jdfasjcv*/) => ({
-    ...data[key] /*{name:"Israel", lastname:"salinas"}*/,
-    key /*key: "user1" || "user2" || "-a-jdfasjcv"*/,
+  let usersArray = keysArray.map((key) => ({
+    ...data[key],
+    key,
   }));
   return usersArray;
 };
 
-const body = document.querySelector("body");
-
+//Card creation
 const createCard = (postData) => {
-  let { img, author, comentarios, date, hashtag, reacciones, relevant, title, avatar, timeread} = postData;
-    hashtag = hashtag.split(" ");
+  const body = document.querySelector("body");
+  let {
+    img,
+    author,
+    comentarios,
+    date,
+    hashtag,
+    reacciones,
+    relevant,
+    title,
+    avatar,
+    timeread,
+  } = postData;
+  hashtag = hashtag.split(" ");
   // Card container
   const cardsContainer = document.querySelector("#allCards");
   const card = document.createElement("div");
@@ -48,7 +60,6 @@ const createCard = (postData) => {
   cardContent.classList.add("card-body", "p-3", "border-light-subtle");
   card.appendChild(cardContent);
 
-
   //First section
   const firstSection = document.createElement("div");
   firstSection.classList.add("d-flex", "align-items-center");
@@ -61,11 +72,7 @@ const createCard = (postData) => {
   firstSection.appendChild(cardUserImage);
 
   const cardUserContent = document.createElement("div");
-  cardUserContent.classList.add(
-    "flex-grow-1",
-    "ms-1",
-    "mt-2",
-  );
+  cardUserContent.classList.add("flex-grow-1", "ms-1", "mt-2");
   firstSection.appendChild(cardUserContent);
 
   const cardUserName = document.createElement("a");
@@ -106,7 +113,7 @@ const createCard = (postData) => {
     "text-decoration-none",
     "text-dark",
     "fw-bold",
-    "link-title",
+    "link-title"
   );
   titleLink.href = "";
   strong.textContent = title;
@@ -130,7 +137,7 @@ const createCard = (postData) => {
     "m-3",
     "d-flex",
     "justify-content-between",
-    "align-items-center",
+    "align-items-center"
   );
   middleContent.appendChild(likesDiv);
 
@@ -164,74 +171,33 @@ const createCard = (postData) => {
   time.textContent = `${timeread} min read`;
   readingTime.appendChild(time);
 };
-
-const printPosts = async () => {
-  let posts = await getAllUsers();
-  console.log(posts);
+// Function to print posts on DOM
+const printPosts = async (getUsers, createCards) => {
+  let posts = await getUsers();
   posts.forEach((post) => {
-    createCard(post);
+    createCards(post);
   });
 };
-printPosts();
+printPosts(getAllUsers, createCard);
 
+// Function to filter posts by relevant = 5
 
-// <div class="card rounded overflow-hidden border-light-subtle">
-//     <img class="w-100" src="https://res.cloudinary.com/practicaldev/image/fetch/s--yH1__SZq--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_775/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ytshyt5ieabbodlgx2gr.png" alt="" />
-//     <div class="card-body p-3 border-light-subtle">
-//         <div class="d-flex align-items-center" id="firstSection">
-//             <img class="rounded-circle me-2" src="https://media.dev.to/cdn-cgi/image/width=90,height=90,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Fuser%2Fprofile_image%2F1463462%2Fadc8bd5d-e040-4aa7-8cce-18cb72dce06c.jpeg" alt="miguelrodriguez99 profile" width="32" height="32" />
-//             <div class="flex-grow-1">
-//                 <a href="#" class="btn btn-ghost text-dark text-start p-1 fs-m">Miguel</a>
-//                 <p class="fw-light fs-sm ms-1 text-light-emphasis link-dark fecha">May 25 (12 hours ago)</p>
-//             </div>
-//         </div>
+const filterPostsByRelevant = async (getUsersfuntion) => {
+  let posts = await getUsersfuntion();
+  const relevantButton = document.getElementById("relevantButton");
+  relevantButton.addEventListener("click", () => {
+    const relevantPosts = posts.filter((post) => post.relevant === 5);
+    console.log(relevantPosts);
+    // send the relevant posts to the createCard function and clear the previous posts
+    const cardsContainer = document.querySelector("#allCards");
+    cardsContainer.innerHTML = "";
+    relevantPosts.forEach((post) => {
+      createCard(post);
+    });
 
-//         <div class="mt-3" id="middleSection">
-//             <h3 class="mb-0">
-//                 <a href="#" class="text-decoration-none text-dark fw-bold link-title"><strong>Frontend resources! &#128640;</strong></a>
-//             </h3>
-//             <div class="tags mt-2">
-//                 <a href="#" class="btn btn-border fs-m">#frontend</a>
-//                 <a href="#" class="btn btn-border fs-m">#tailwindcss</a>
-//                 <a href="#" class="btn btn-border fs-m">#react</a>
-//                 <a href="#" class="btn btn-border fs-m">#javascript</a>
-//             </div>
-
-//             <div class="likes mt-3 d-flex justify-content-between align-items-center">
-//                 <a href="#" class="btn btn-ghost fs-reactions d-flex gap-1">
-//                     <span>33 reactions</span>
-//                 </a>
-//                 <a href="#" class="btn btn-ghost fs-reactions">
-//                     <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="crayons-icon">
-//                         <path d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z"></path>
-//                     </svg>
-//                     4 comments
-//                 </a>
-//                 <div class="d-flex align-items-center">
-//                     <p class="fw-light fs-sm text-light-emphasis mb-0">5 min read</p>
-//                     <svg class="btn btn-ghost-morado p-2 ms-1" viewbox="0 0" width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-//                         <path d="M6.75 4.5h10.5a.75.75 0 01.75.75v14.357a.375.375 0 01-.575.318L12 16.523l-5.426 3.401A.375.375 0 016 19.607V5.25a.75.75 0 01.75-.75zM16.5 6h-9v11.574l4.5-2.82 4.5 2.82V6z"></path>
-//                     </svg>
-//                 </div>
-//             </div>
-//         </div>
-
-//         <div class="d-flex mt-3 gap-2" id="commentSection">
-//             <div class="avatarcomment">
-//                 <img class="rounded-circle" src="https://media.dev.to/cdn-cgi/image/width=320,height=320,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Fuser%2Fprofile_image%2F1493941%2F935afd13-e2f6-480b-9fd3-975988dc6f30.jpg" alt="" width="24" height="24" />
-//             </div>
-//             <div class="comment rounded bg-fondo flex-grow-1 p-3">
-//                 <div class="authortime d-flex align-items-center gap-1">
-//                     <a href="#" class="text-decoration-none text-light-emphasis fs-m">John</a>
-//                     <a href="#" class="fs-reactions text-decoration-none text-light-emphasis fw-lighter">4 hours ago</a>
-//                 </div>
-//                 <div class="fs-m">
-//                     <p>Awesome!</p>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-// </div>
+  });
+};
+filterPostsByRelevant(getAllUsers);
 
 // // Function to filter posts by a given user
 // const filterPostsByUser = (posts, userName) => {
